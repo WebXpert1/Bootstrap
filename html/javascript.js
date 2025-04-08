2365d892c9bbc976fe5dbeb6f4a50cb654b23d19
Ayesha <ayeshanadeem403026@gmail.com>
2025-04-08 12:32:15 +0500

    8 of april

---
+++ html/javascript.js
@@ -0,0 +1,51 @@
+// {let a='hello World </br>' ;
+// document.write(a)}
+
+// {let b='hello World 2 </br>';
+// document.write(b)}
+
+// let c='Hello World 3';
+// document.write(c);
+// // console.log(c)
+// // alert(c)
+
+
+
+// const d='This is const';
+//     d='Over the value';
+    
+
+//    //function component
+
+//    {  
+//         var x="Black Box";
+//         // console.log(x)
+//     }
+
+
+//   var a='New values of let';
+//     var x='Black Box Movie';
+// console.log(typeof(a))
+
+//boolean
+
+
+    // let abc='Bigint';
+    //     abc=false
+
+{
+    var x='in a block';
+    var y= 1234 //number
+    var z= BigInt(6329876543213809876543); //bignit
+    var xyz=false //bolean
+    var f= null
+    var un //undefined
+    un=["string", "second", true, 12345]; //array
+    var ob = {
+        name:Raya,
+        married:false,
+        age: 15,
+        arr:["value1", "value2"]  //object
+    }
+console.log(typeof(z))
+}
